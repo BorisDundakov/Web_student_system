ae1ebaef3f1d547718f54340d637b5c8fb32e90f
Boris Dundakov <boris.stefan.de@gmail.com>
2022-07-18 19:26:19 +0300

    sql connection

--- Course.py
+++ Course.py
@@ -1,5 +1,13 @@
 class Course:
-    def __init__(self, course_id, course_name, course_credits):
+    def __init__(self, course_id, course_name, course_credits, course_description):
         self._course_id = course_id
         self._course_name = course_name
         self.course_credits = course_credits
+        self._course_description = course_description
+
+    def get_professor_id(self):
+        pass
+
+    def add_course(self, course_info):
+        # insert into Courses(course_info + professor_id)
+        pass
--- SQL.py
+++ SQL.py
@@ -1,52 +1,76 @@
-import pyodbc
+import psycopg2
 
 
-def read(connection):
-    print("Read")
-    cursor = connection.cursor()
-    cursor.execute("select * from Dummy")
-    for row in cursor:
-        print(f'row = {row}')
-    print()
+def connect():
+    conn = None
+    cursor = None
+
+    conn = psycopg2.connect(
+        database="DB_WebStudent",
+        user='postgres',
+        password='postgres123',
+        host='localhost',
+        port=5432
+    )
+    cursor = conn.cursor()
+    script = 'SELECT * FROM "UserTypes"'
+    cursor.execute(script)
+    print(cursor.fetchall())
+
+    try:
+        conn = psycopg2.connect(
+            database="DB_WebStudent",
+            user='postgres',
+            password='postgres123',
+            host='localhost',
+            port=5432
+        )
+        cursor = conn.cursor()
+        script = 'SELECT * FROM "UserTypes"'
+        # script = 'SELECT * FROM "DB_WebStudent".public.tables.UserTypes'
+        cursor.execute(script)
+        print(cursor.fetchall())
+
+    except Exception as error:
+        print("Error! Please check database configuration!")
+
+    finally:
+        if conn:
+            conn.close()
+        if cursor:
+            cursor.close()
 
 
-def create(connection):
-    print("Create")
-    cursor = connection.cursor()
-    cursor.execute("insert into Dummy (a,b) values (?,?);",
-                   3232, 'catzzz')
-
-    connection.commit()
-    read(connection)
+def insert_data(data):
+    # insert_script = 'INSERT INTO Users'
+    pass
 
 
-def update(connection):
-    print("Update")
-    cursor = connection.cursor()
-    cursor.execute("update Dummy set b = ? where a = ?;",
-                   'dogzzz', 3232)
-
-    connection.commit()
-    read(connection)
+def fetch_all_data():
+    pass
 
 
-def delete(connection):
-    print("Delete")
-    cursor = connection.cursor()
-    cursor.execute("delete from Dummy where a > 5")
-    connection.commit()
-    read(connection)
+connect()
 
-
-connection = pyodbc.connect(
-    "Driver ={ODBC Driver 11 for SQL Server};"
-    "Server=HOME;"
-    "Database=WebStudent DB;"
-    "Trusted_Connection = yes;)"
-)
-
-create(connection)
-update(connection)
-delete(connection)
-
-connection.close()
+#
+# def read(connection):
+#     print("Read")
+#
+#     cursor = connection.cursor()
+#     cursor.execute("select * from Dummy")
+#     for row in cursor:
+#         print(f'row = {row}')
+#     print()
+#
+#
+# def create(connection):
+#     pass
+#
+#
+# def update(connection):
+#     pass
+#
+#
+# def delete(connection):
+#     print("Delete")
+#     pass
